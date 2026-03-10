8c1cf4b7dd337b7baf51930855b55e698ad43100
jackatlas5706 <ryan.haug@students.mot.k12.de.us>
2026-03-10 11:41:03 -0400

    Create GameObject class with basic properties

---
+++ GameObject.js
@@ -0,0 +1,11 @@
+class GameObject {
+  constructor(x, y, width, height) {
+    this.name = this.constructor.name;
+    this.x = x; this.y = y;
+    this.width = width; this.height = height;
+    this.alive = true;
+  }
+  update(dt) {}
+  draw(ctx) {}
+  getBounds() { return { x: this.x, y: this.y, width: this.width, height: this.height }; }
+}
